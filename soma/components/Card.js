13fa8804866df5a83d9a253ae93e762dedc3295a
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-20 03:00:29 +0530

    relocated Card.js ad updated tailwind.config.js with new colors

--- soma/components/Card.js
+++
@@ -1,9 +0,0 @@
-// Creating our first ever component which can be reused
-
-export default function Card({children}) {
-    return (
-        <div className="bg-white shadow-sm">
-            {children}
-        </div>
-    )
-}
