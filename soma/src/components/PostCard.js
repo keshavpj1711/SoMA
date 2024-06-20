2cc82e21d9f6f12b2555f4fb4dede0a29acb4a17
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-20 22:50:18 +0530

    done with the button to intereact with the post

--- soma/src/components/PostCard.js
+++ soma/src/components/PostCard.js
@@ -7,17 +7,17 @@ export default function PostCard() {
 
             {/* This is for header kind of thing for our post */}
             <div className="flex gap-3">
-            <div>
-                <Avatar />
+                <div>
+                    <Avatar />
+                </div>
+
+                <div>
+                    <p><a href="" className="font-semibold">Rhaenyra Targaryen</a> shared a <a href="" className="text-socialBlue">post</a></p>
+                    <p className="text-gray-500 text-sm">1 hour ago</p>
+                </div>
             </div>
 
-            <div>
-                <p><a href="" className="font-semibold">Rhaenyra Targaryen</a> shared a <a href="" className="text-socialBlue">post</a></p>
-                <p className="text-gray-500 text-sm">1 hour ago</p>
-            </div>
-            </div>
-
-            {/* Actual thing for our post */}
+            {/* Actual Content of our post */}
             <div className="">
                 <p className="my-3">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam omnis aut fuga exercitationem nulla similique, ex a optio quis modi fugiat, beatae eius incidunt possimus. Fugit nihil molestias illo cumque!
@@ -26,6 +26,30 @@ export default function PostCard() {
                 <div className="rounded-md overflow-hidden">
                     <img src="https://images.unsplash.com/photo-1592335829159-3630b01803f7?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
+            </div>
+
+            {/* The major buttons to intereact with post */}
+            <div className="flex gap-8 mt-4">
+                <button className="flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    </svg>
+                    12
+                </button>
+
+                <button className="flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                    </svg>
+                    5
+                </button>
+
+                <button className="flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                    </svg>
+                    Share
+                </button>
             </div>
         </Card>
     )
