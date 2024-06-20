a491d05ddc86ec07c08131e030b6913637f233da
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-20 23:08:47 +0530

    added a more button to header of PostCard

--- soma/src/components/PostCard.js
+++ soma/src/components/PostCard.js
@@ -11,11 +11,23 @@ export default function PostCard() {
                     <Avatar />
                 </div>
 
-                <div>
+                <div className="grow">
                     <p><a href="" className="font-semibold">Rhaenyra Targaryen</a> shared a <a href="" className="text-socialBlue">post</a></p>
                     <p className="text-gray-500 text-sm">1 hour ago</p>
                 </div>
+
+                {/* Getting that 3 dot menu thing */}
+                <div>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                        </svg>
+                    </button>
+                </div>
             </div>
+
+
+
 
             {/* Actual Content of our post */}
             <div className="">
