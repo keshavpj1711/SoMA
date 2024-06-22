4c7fc2979be3c7efccc1af5183d215f39e80f27a
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-23 03:28:06 +0530

    small design change in Navigation.js

--- soma/src/components/NavigationCard.js
+++ soma/src/components/NavigationCard.js
@@ -14,7 +14,7 @@ export default function NavigationCard() {
     const pathName = usePathname();
     console.log({pathName});
 
-    const activeElement = 'flex gap-3 py-4 bg-socialBlue text-white -mx-10 rounded-md px-10 transition-all hover:shadow-md shadow-gray-300'
+    const activeElement = 'flex gap-3 py-3 my-1 bg-socialBlue text-white -mx-10 rounded-md px-10 transition-all hover:shadow-md shadow-gray-300'
     const nonActiveElement = 'flex gap-3 py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'
 
     return (
@@ -24,7 +24,7 @@ export default function NavigationCard() {
                 Navigation
             </h2>
 
-            {/* Conditions to only trigger the activeElement class when the pathName is required one */}
+            {/* Conditions to only trigger the activeElement class when the pathName is the required one */}
             <Link href="/" className={pathName== "/"? activeElement : nonActiveElement}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
