bb50a78d75605e9b9925ac73c1bd426bd21ebf9b
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-22 13:53:11 +0530

    added Layout.js to use our major design everywhere

---
+++ soma/src/components/Layout.js
@@ -0,0 +1,20 @@
+import NavigationCard from "@/components/NavigationCard";
+import Image from "next/image";
+import Card from "@/components/Card";
+import PostFormCard from "@/components/PostFormCard";
+import Avatar from "@/components/Avatar";
+
+export default function Layout({children}) {
+    return (
+        <div className="flex mt-4 max-w-4xl mx-auto gap-6">
+
+			<div className="w-1/4">
+				<NavigationCard />
+			</div>
+
+			<div className="grow">		
+                {children}
+			</div>
+		</div>
+    )
+}
