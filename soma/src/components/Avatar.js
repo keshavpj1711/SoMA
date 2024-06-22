682772cf9f3f9add991b16fc15edece22ee24d6e
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-22 14:50:44 +0530

    added an option for default and large avatar size

--- soma/src/components/Avatar.js
+++ soma/src/components/Avatar.js
@@ -1,9 +1,18 @@
 // We are placing this component seperately bcoz 
 // Avatars will be be very common and will shown over every posts
 
-export default function Avatar() {
+export default function Avatar({size}) {
+    
+    let width = "w-12"
+
+    if (size == "lg") {
+        width = "w-28"
+    }
+    
     return (
-        <div className="w-12 rounded-full overflow-hidden">
+        // The issue is with the string interpolation syntax. 
+        // In JSX, you need to use backticks (`) for template literals instead of single quotes 
+        <div className={`rounded-full overflow-hidden ${width}`}>
             <img src="https://avatarfiles.alphacoders.com/334/334259.jpg" alt="" />
         </div>
     )
