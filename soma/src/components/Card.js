6468c26a6f1127988e5c7877d0269168837d150c
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-22 14:51:16 +0530

    added an option to also include noPadding

--- soma/src/components/Card.js
+++ soma/src/components/Card.js
@@ -1,9 +1,16 @@
 // Creating our first ever component which can be reused
-export default function Card({children}) {
+export default function Card({children, noPadding}) {
+    
+    let classes = "bg-white shadow-md shadow-gray-200 rounded-md mb-5"
+
+    if (!noPadding) {
+        classes += " p-4";
+    }
+
     return (
         // So basically whatever we put in <Card></Card> will, 
         // have all the benefits of this div layed out below
-        <div className="bg-white shadow-md shadow-gray-200 p-4 rounded-md mb-5">
+        <div className={classes}>
             {children}
         </div>
     )
