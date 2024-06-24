fedff6a59f6ad46599484c0f5600ed0620da44d0
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-25 02:41:22 +0530

    making the Layout responsive so that navigationcard adjusts

--- soma/src/components/Layout.js
+++ soma/src/components/Layout.js
@@ -5,16 +5,26 @@ import PostFormCard from "@/components/PostFormCard";
 import Avatar from "@/components/Avatar";
 
 export default function Layout({ children, hideNavigation }) {
+    let rightColumnClasses = ''
+    if (hideNavigation) {
+        rightColumnClasses += 'w-full'
+    } else {
+        // Below line basically means that for medium and above screen width that is for desktop
+        // make x-margin to be zero and width to 3/4
+        // But for smaller screen make x-margin 4
+        rightColumnClasses += 'mx-4 md:mx-0 md:w-3/4'
+    }
+    
     return (
-        <div className="flex mt-4 max-w-4xl mx-auto gap-6">
+        <div className="md:flex mt-4 max-w-4xl mx-auto gap-6">
 
             {!hideNavigation && (
-                <div className="w-1/4">
+                <div className="fixed md:static bottom-0 w-full md:w-1/4 -mb-5">
                     <NavigationCard />
                 </div>
             )}
 
-            <div className={hideNavigation ? "w-full" : "w-3/4"}>
+            <div className={rightColumnClasses}>
                 {children}
             </div>
         </div>
