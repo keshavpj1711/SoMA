30d4733fd2b5d16c7d8644a6abc8768892dacba0
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-24 17:05:56 +0530

    done with saved posts page

--- soma/src/app/saved/page.js
+++ soma/src/app/saved/page.js
@@ -1,9 +1,18 @@
+"use client";
 import Layout from "../../components/Layout";
+import PostCard from "../../components/PostCard";
 
-export default function SavedPost() {
+export default function SavedPostPage() {
     return (
         <Layout>
+            <h1 className="text-5xl text-gray-400 font-bold mt-2 mb-4 mx-2">
+                Saved Posts
+            </h1>
+
+            <PostCard />
             
+            <PostCard />
+
         </Layout>
     )
 }
