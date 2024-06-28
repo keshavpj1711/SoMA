a91a27689c679ea82c2caaf49a8ca6f10c27b542
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-29 03:11:14 +0530

    got the logout button working and redirecting to login page

--- soma/src/components/NavigationCard.js
+++ soma/src/components/NavigationCard.js
@@ -1,4 +1,5 @@
 "use client";
+import { useAuth } from '../supabase/AuthContext';
 import { usePathname, useRouter } from "next/navigation";
 // Faced an error when using next/router for using useRouter
 // The problem was due to changes in nextjs 13
@@ -13,6 +14,8 @@ export default function NavigationCard() {
     // Also since Nextjs 13 useRouter was shifted to next/navigation instead of use/Router 
     const pathName = usePathname();
     console.log({ pathName });
+
+    const { logOut } = useAuth()
 
     const activeElement = 'flex gap-1 md:gap-3 py-3 my-1 bg-socialBlue text-white -mx-4 md:-mx-8 rounded-md px-6 md:px-10 transition-all hover:shadow-md shadow-gray-300'
     const nonActiveElement = 'flex gap-1 md:gap-3 py-2 my-2 px-6 hover:bg-blue-500 hover:bg-opacity-20 -mx-4 hover:mx-0 md:px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'
@@ -53,12 +56,12 @@ export default function NavigationCard() {
                     <span className="hidden md:block">Navigation</span>
                 </Link>
 
-                <Link href="" className={nonActiveElement}>
+                <button onClick={logOut} className={nonActiveElement}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
                     </svg>
                     <span className="hidden md:block">Logout</span>
-                </Link>
+                </button>
             </div>
         </Card>
     )
