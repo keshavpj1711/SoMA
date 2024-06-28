d565c3ecc193585c8be8327705840a4af1fab0c0
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-29 02:15:56 +0530

    added login using google auth

--- soma/src/app/login/page.js
+++ soma/src/app/login/page.js
@@ -1,5 +1,6 @@
+"use client"
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useAuth } from '../../supabase/AuthContext';
 import { supabase } from '../../supabase/supabaseClient';
 import Card from "../../components/Card";
@@ -7,11 +8,11 @@ import Layout from "../../components/Layout";
 
 export default function Login() {
     const { user } = useAuth();
-    const router = useRouter();
+    const router = useRouter()
 
     useEffect(() => {
         if (user) {
-            router.push('/profile');
+            router.push('/');
         }
     }, [user]);
 
@@ -22,6 +23,8 @@ export default function Login() {
         if (error) console.log('Error logging in with Google:', error.message);
     };
 
+    const buttonClasses = "flex gap-4 p-4 items-center w-full justify-center border-b border-b-gray-100 hover:bg-socialBlue hover:text-white hover:border-b-socialBlue transition-all hover:scale-110"
+
     return (
         <Layout hideNavigation={true}>
             <div className="h-screen flex items-center">
@@ -31,26 +34,26 @@ export default function Login() {
                     </h1>
                     <Card noPadding={true}>
                         <div className="rounded-md overflow-hidden">
-                            <a href="/" className="flex gap-4 p-4 items-center justify-center border-b border-b-gray-100 hover:bg-socialBlue hover:text-white transition-all hover:border-b-socialBlue hover:scale-110">
+                            <button onClick={handleGoogleLogin} className={buttonClasses}>
                                 <svg className="h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
                                     <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
                                 </svg>
                                 Login with Google
-                            </a>
+                            </button>
 
-                            <a href="/" className="flex gap-4 p-4 items-center justify-center border-b border-b-gray-100 hover:bg-socialBlue hover:text-white hover:border-b-socialBlue transition-all hover:scale-110">
+                            {/* <button onClick={''} className={buttonClasses}>
                                 <svg className="h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
                                     <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
                                 </svg>
                                 Login with Github
-                            </a>
+                            </button>
 
-                            <a href="/" className="flex gap-4 p-4 items-center justify-center border-b border-b-gray-100 hover:bg-socialBlue hover:text-white hover:border-b-socialBlue transition-all hover:scale-110">
+                            <button onClick={''} className={buttonClasses}>
                                 <svg className="h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
                                     <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
                                 </svg>
                                 Login with Google
-                            </a>
+                            </button> */}
                         </div>
                     </Card>
                 </div>
